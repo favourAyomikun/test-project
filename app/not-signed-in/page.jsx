6af9016bd23f45c8d0dd402ff05b3bef04c195d4
Oshoaro Favour <oshoarofavour@gmail.com>
2024-11-23 12:50:58 +0100

    adjusted the design

--- app/not-signed-in/page.jsx
+++ app/not-signed-in/page.jsx
@@ -112,7 +112,7 @@ const NotSignedInPage = () => {
         <NotSignedInSidebar />
 
         {/* Main content */}
-        <div className="flex-1 justify-center h-full flex flex-col mt-4 md:mt-0">
+        <div className="flex-1 justify-center h-full flex flex-col mt-5 md:mt-0">
           {/* Header */}
           <div className="flex justify-center p-8">
             <div className="text-center">
--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ const SignedInPage = () => {
         <SignedInSidebar />
 
         {/* Main content */}
-        <div className="flex-1 justify-center min-h-screen flex flex-col mt-4 md:mt-0">
+        <div className="flex-1 justify-center h-full flex flex-col mt-5 md:mt-0">
           {/* Header */}
           <div className="flex justify-center p-8">
             <div className="text-center">
